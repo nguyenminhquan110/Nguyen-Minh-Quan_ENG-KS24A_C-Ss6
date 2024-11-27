483d01973f4d8b1e02dc8e58441949e88d374f2c
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-27 23:51:38 +0700

    Add files via upload

---
+++ Ss6_8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int month;
+	float tien_goc; 
+	float  lai_suat = 0.01;
+	float tien_lai; 
+    float tien_nhan;
+    printf("Nhap so tien ban muon gui: ");
+	scanf("%f", &tien_goc); 
+	printf("Moi ban nhap so thang muon gui: ");
+	scanf("%f", &month);
+	for(int month = 1; month <= n; month ++){
+		tien_lai = tien_goc * lai_suat * month; 
+	}
+	 tien_nhan = tien_goc + tien_lai; 
+	printf("Tien lai cua ban la: %.3f\n", tien_lai = tien_nhan - 1000);
+	printf("So tien ban nhan duoc la: %.3f\n", tien_nhan);
+	
+	return 0; 
+}
+
